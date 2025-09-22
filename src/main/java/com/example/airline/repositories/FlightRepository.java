f49371278366f6777f163777428238f0358b47f6
ENDERSOUL5 <joseaguilardelahoz58@gmail.com>
2025-09-22 16:53:00 -0500

    2 Queries realizados pero tengo que comprobarlos, faltan 2 para terminar

--- src/main/java/com/example/airline/repositories/FlightRepository.java
+++ src/main/java/com/example/airline/repositories/FlightRepository.java
@@ -1,8 +1,23 @@
 package com.example.airline.repositories;
 
 import com.example.airline.entities.Flight;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.awt.print.Pageable;
+import java.util.List;
 
 public interface FlightRepository extends JpaRepository<Flight, Long> {
-    Flight findFlightsByid(long id);
+    List<Flight> findFlightByByAirlinebyNameIgnoreCase(String airlineName, Pageable pageable);
+
+    @Query("""
+    select f from Flight f where f.originAirport.code = :Origen and f.destinationAirport.code = :Destino
+""")
+    Page<Flight> findFlightByOriginAndDestinationFlighs(@Param("Origen") String codeOrigen,
+                                                        @Param("Destino") String codeDestino, Pageable pageable
+    );
+
+    // Revisar la segunda consulta
 }
