61e387c7fa1f8b32d3c19911a7b3cf95b7e9c52d
ENDERSOUL5 <joseaguilardelahoz58@gmail.com>
2025-09-26 07:56:40 -0500

    Todos los mappers de SeatInvetory agregados

--- src/main/java/com/example/airline/Mappers/SeatInventoryMapper.java
+++ src/main/java/com/example/airline/Mappers/SeatInventoryMapper.java
@@ -18,14 +18,38 @@ public class SeatInventoryMapper {
         Flight foundFlight = flightRepository.findById(createRequest.flightId())
                 .orElseThrow(() -> new EntityNotFoundException("Vuelo con el ID: " + createRequest.flightId() + "no encontrado"));
 
-        SeatInventory seatInventory = new SeatInventory().builder().availableSeats(createRequest.availableSeats()).totalSeats(createRequest.totalSeats()).cabin(createRequest.cabin()).flight(foundFlight).build();
-        return seatInventoryRepository.save(seatInventory);
+        return new SeatInventory().builder().availableSeats(createRequest.availableSeats()).totalSeats(createRequest.totalSeats()).cabin(createRequest.cabin()).flight(foundFlight).build();
     }
 
-    public boolean updateSeatInventory(SeatInvetoryDTO.seatInventoryUpdateRequest updateRequest){
-        Flight foundFlight = flightRepository.findById(updateRequest.flightID())
-                .orElseThrow(() -> new EntityNotFoundException("El vuelo con el ID: " + updateRequest.flightID() + "no fue encontrado"));
-        // Continuar, ahora se evalua si cada parametro que entra es diferente a null, si lo es se edita esa informacion en el objeto
+    public SeatInventory updateSeatInventory(SeatInvetoryDTO.seatInventoryUpdateRequest updateRequest){
+        SeatInventory foundSeatInventory = seatInventoryRepository.findById(updateRequest.seatInventoryId())
+                .orElseThrow(() -> new EntityNotFoundException("Seat inventoru con ID: " + updateRequest.seatInventoryId() + "no encontrado"));
+
+       if (updateRequest.availableSeats()!= null){
+           foundSeatInventory.setAvailableSeats(updateRequest.availableSeats());
+       }
+       if (updateRequest.totalSeats()!= null){
+           foundSeatInventory.setTotalSeats(updateRequest.totalSeats());
+       }
+       if (updateRequest.cabin()!= null){
+           foundSeatInventory.setCabin(updateRequest.cabin());
+       }
+       if (updateRequest.flightID()!= null){
+           Flight foundFlight = flightRepository.findById(updateRequest.flightID())
+                   .orElseThrow(() -> new EntityNotFoundException("Vuelo con ID: " + updateRequest.flightID() + "no encontrado"));
+           foundSeatInventory.setFlight(foundFlight);
+       }
+       return seatInventoryRepository.save(foundSeatInventory);
+
+    }
+
+    public static SeatInvetoryDTO.seatInventoryDtoResponse seatInventoryResponse(SeatInventory seatInventory){
+        if (seatInventory.getAvailableSeats() == null){
+            return null;
+        }
+        return new SeatInvetoryDTO.seatInventoryDtoResponse(seatInventory.getId(),
+                seatInventory.getTotalSeats(), seatInventory.getAvailableSeats(), seatInventory.getCabin(),
+                seatInventory.getFlight().getId());
     }
 
 }
